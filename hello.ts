6a9fbd4446f0e92fe81c687f5c068f9be7dc65b4
sujit gouda <sujit.gda997@gmail.com>
2025-05-20 20:19:37 +0530

    updated code

--- hello.ts
+++ hello.ts
@@ -3,3 +3,9 @@ const add = (a: number, b: number): number => {
 };
 
 console.log("Sum:", add(3, 4));
+
+const sub = (a: number, b: number): number => {
+  return a - b;
+};
+
+console.log("difference:", sub(10, 22));
